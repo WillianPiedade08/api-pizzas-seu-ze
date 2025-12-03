0c2ea4eaa79ab42c9d6925ded6cabf9c010e23c1
WillianPiedade08 <willianpiedade17@gmail.com>
2025-12-03 14:58:00 -0300

    rota

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const cors = require('cors')
 
 const port = process.env.PORT || 5000
 const app = express()
-const routes = require('./src/routes')
+const rota = require('./src/routes')
 
 app.use(cors())
 app.use(express.json())
-app.use(routes)
+app.use(rota)
 
 app.listen(port, () => {
     console.log(`Server respondendo em http://localhost:${port}`)
